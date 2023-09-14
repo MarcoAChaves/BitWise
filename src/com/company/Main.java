16adbd83482e11552102e73991024cc4abe405f9
Marco Chaves <macschaves@gmail.com>
2023-09-14 15:35:13 -0300

    Operadores BitWise

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int n1 = 89;
+        int n2 = 60;
+
+        System.out.println(n1&n2);
+        System.out.println(n1|n2);
+        System.out.println(n1^n2);
+
+        // & operação "E" bit a bit - | operação "OU" bit a bit - ^ operação "OU-exclusivo" bit a bit
+    }
+}
